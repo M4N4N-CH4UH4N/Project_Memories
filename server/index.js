ee97967735d9b652d828b9b0df0285182761d39c
M4N4N-CH4UH4N <mananchauhan6396@gmail.com>
2023-10-19 14:28:16 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 // const dotenv = require("dotenv");
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 
@@ -19,7 +21,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://mananchauhan:manan1234@cluster0.bdtyufc.mongodb.net/memories_app?retryWrites=true&w=majority';
+
+
+const CONNECTION_URL = process.env.URL;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
